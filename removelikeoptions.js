a6479dfaf8a295de14c88c94036d1d34c2c6bab5
Rohit Nigam <cool.rohit.im@gmail.com>
2016-02-27 10:03:39 +0530

    first commit

---
+++ removelikeoptions.js
@@ -0,0 +1,16 @@
+$(document).load(function() {
+
+
+setTimeout(byebyeemotions, 10000);
+
+  });
+
+
+function byebyeemotions() {
+
+  var nomorelikes = document.getElementsByClassName('_1oxj');
+  for (var i = 0; i < nomorelikes.length; i ++) {
+      nomorelikes[i].style.display = 'none';
+    }
+
+}
